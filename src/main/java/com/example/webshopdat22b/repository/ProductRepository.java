2e8f5deb3acc8d56e1c6916e5182a8ffa89745c9
Lasse Lysskov <l.lysskov@gmail.com>
2023-03-17 15:28:23 +0100

    Repository Product

---
+++ src/main/java/com/example/webshopdat22b/repository/ProductRepository.java
@@ -0,0 +1,41 @@
+package com.example.webshopdat22b.repository;
+import com.example.webshopdat22b.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    //database-properties
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/webshopdat22b";
+    private final static String UID = "root";
+    private final String PWD = "LabanKenzo12";
+
+    public List<Product> getAll() {
+        List<Product> productList = new ArrayList<>();
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            Statement statement = connection.createStatement();
+            final String SQL_QUERY = "SELECT * FROM Products";
+            ResultSet resultSet = statement.executeQuery(SQL_QUERY);
+            while(resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                double price = resultSet.getDouble(3);
+                Product product = new Product(id, name, price);
+                productList.add(product);
+                System.out.println(product);
+            }
+        }
+        catch(SQLException e)
+        {
+           System.out.println("Could not query database");
+           e.printStackTrace();
+        }
+
+        return productList;
+    }
+}
