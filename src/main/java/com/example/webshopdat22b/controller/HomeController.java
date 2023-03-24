99208693419d7c93dcf134c6dcc3153a82b74ecb
Lasse Lysskov <l.lysskov@gmail.com>
2023-03-24 11:39:59 +0100

    Add product added

--- src/main/java/com/example/webshopdat22b/controller/HomeController.java
+++ src/main/java/com/example/webshopdat22b/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.example.webshopdat22b.controller;
 
+import com.example.webshopdat22b.model.Product;
 import com.example.webshopdat22b.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -18,5 +21,23 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("products", productRepository.getAll());
        return "index";
+    }
+    //fra anchor i index
+    @GetMapping("/create")
+    public String showCreate() {
+        //vis create side
+        return "create";
+    }
+    //fra form action
+    @PostMapping("/create")
+    public String createProduct(@RequestParam("product-name") String newName, @RequestParam("product-price") double newPrice) {
+        //lave et nyt Product
+        Product newProduct = new Product();
+        newProduct.setName(newName);
+        newProduct.setPrice(newPrice);
+
+        //gem nyt produkt
+        productRepository.addProduct(newProduct);
+        return "redirect:/";
     }
 }
