5a44718923b558cde3491d5f3cef0fa3f5798fe9
Lasse Lysskov <l.lysskov@gmail.com>
2023-03-27 12:57:24 +0200

    Delete and update added

--- src/main/java/com/example/webshopdat22b/controller/HomeController.java
+++ src/main/java/com/example/webshopdat22b/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.example.webshopdat22b.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,6 +39,40 @@ public class HomeController {
 
         //gem nyt produkt
         productRepository.addProduct(newProduct);
+        return "redirect:/";
+    }
+
+
+    //vis update side for produkt ud fra parameter i url
+    @GetMapping("/update/{id}")
+    public String showUpdate(@PathVariable("id") int updateId, Model model) {
+        //find produkt med id=updateId i database
+        Product updateProduct = productRepository.findProductById(updateId);
+
+        //tilføj produkt til viewmodel, så det kan bruges i Thymeleaf
+        model.addAttribute("product", updateProduct);
+
+        //fortæl Spring hvilken HTML-side der skal vises
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String updateProduct(@RequestParam("product-name") String updateName,
+                                @RequestParam("product-price") double updatePrice,
+                                @RequestParam("product-id") int updateID) {
+        //lave produkt ud fra parametre
+        Product updateProduct = new Product(updateID, updateName, updatePrice);
+
+        //kald opdater i repository
+        productRepository.updateProduct(updateProduct);
+
+        //redigerer til oversigtssiden
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") int deleteId) {
+        productRepository.deleteProduct(deleteId);
         return "redirect:/";
     }
 }
--- src/main/java/com/example/webshopdat22b/repository/ProductRepository.java
+++ src/main/java/com/example/webshopdat22b/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.example.webshopdat22b.repository;
 import com.example.webshopdat22b.model.Product;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -10,9 +11,13 @@ import java.util.List;
 public class ProductRepository {
 
     //database-properties
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/webshopdat22b";
-    private final static String UID = "root";
-    private final String PWD = "LabanKenzo12";
+
+    @Value("${spring.datasource.url}")
+    private String DB_URL;
+    @Value("${spring.datasource.username}")
+    private String UID;
+    @Value("${spring.datasource.password}")
+    private String PWD;
 
     public List<Product> getAll() {
         List<Product> productList = new ArrayList<>();
@@ -43,7 +48,7 @@ public class ProductRepository {
         try {
             //connect to database
             Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
-            final String CREATE_QUERY = "INSERT INTO products(name, price) VALUES(?, ?)";
+            final String CREATE_QUERY = "INSERT INTO Products(name, price) VALUES(?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(CREATE_QUERY);
 
             //set attributer i prepared statement
@@ -55,6 +60,88 @@ public class ProductRepository {
         }
         catch(SQLException e) {
             System.out.println("Could not create product");
+            e.printStackTrace();
+        }
+    }
+    public void updateProduct(Product product) {
+        try {
+            //SQL statement
+            final String UPDATE_QUERY = "UPDATE Products SET name = ?, price = ? WHERE id = ?";
+
+            //Connect to DB
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+
+            //prepared statement
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+
+            //set parameters
+            String name = product.getName();
+            double price = product.getPrice();
+            int id = product.getId();
+            preparedStatement.setString(1, name);
+            preparedStatement.setDouble(2, price);
+            preparedStatement.setInt(3, id);
+
+            //execute query
+            preparedStatement.executeUpdate();
+        }
+        catch(SQLException e)
+        {
+           System.out.println("Could not update the product");
+           e.printStackTrace();
+        }
+    }
+
+    public Product findProductById(int id) {
+        Product foundProduct = new Product();
+        foundProduct.setId(id);
+        try {
+            //SQL statement
+            final String SQL_QUERY = "SELECT * FROM Products WHERE id = ?";
+
+            //Connect to database
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+
+            //prepared statement
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_QUERY);
+
+            //set parameters
+            preparedStatement.setInt(1, id);
+
+            //execute statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //få product ud af resultset
+            resultSet.next();
+            String name = resultSet.getString(2);
+            double price = resultSet.getDouble(3);
+            foundProduct.setName(name);
+            foundProduct.setPrice(price);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Could not query database");
+            e.printStackTrace();
+        }
+        return foundProduct;
+    }
+
+    public void deleteProduct(int id) {
+        try {
+            //SQL statement
+            final String DELETE_QUERY = "DELETE FROM Products WHERE id = ?";
+            //Connect to database
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            //prepared statement
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
+            //set parameters
+            preparedStatement.setInt(1, id);
+            //execute statement
+            preparedStatement.executeQuery();
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Could not delete the product");
             e.printStackTrace();
         }
     }
